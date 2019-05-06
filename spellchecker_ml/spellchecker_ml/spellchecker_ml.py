17451223c09d63a236db90beeacb27c620410265
Eric Schles <ericschles@gmail.com>
2019-05-05 20:26:45 -0400

    adding fixes

--- spellchecker_ml/spellchecker_ml/spellchecker_ml.py
+++ spellchecker_ml/spellchecker_ml/spellchecker_ml.py
@@ -1,72 +1,35 @@
 from spellchecker import SpellChecker as SpellChecker
-from text_processing_ml.normalization import NormalizeText
-import nltk
-import numpy as np
-import sys, json, codecs, pickle, random, time
-from datetime import datetime
-from sklearn.externals import joblib
-from sklearn.preprocessing import LabelEncoder
-from hmmlearn import hmm
 import autocomplete
+import random
 
 class SpellCheckerML:
-    def __init__(self, label_encoder=None):
-        self.label_encoder = label_encoder
+    def __init__(self):
         self._spell_checker = SpellChecker()
+        self.autocomplete = autocomplete
         
-    def encode_text(self, text):
-        lines = [line.split(' ') for line in text.split("\n")]
-        words = []
-        for line in lines:
-            for word in line:
-                words.append(word.lower())
-        alphabet = set(words)
-        if not self.label_encoder:
-            self.label_encoder = LabelEncoder()
-            self.label_encoder.fit(list(alphabet))
-        seq = self.label_encoder.transform(words)
-        features = np.fromiter(seq, np.int64)
-        features = np.atleast_2d(features).T
-        lengths = [len(line) for line in lines]
-        return features, lengths
-    
-    def train_hmm(self, text, num_states):
-        np.random.seed(seed=42)
-        features, lengths = self.encode_text(text)
-        self.model = hmm.MultinomialHMM(n_components=num_states, init_params='ste')
-        self.model.fit(features, lengths)
-
-    def save_model(self):
-        joblib.dump(self.model, "hmm_model.pkl")
+    def train(self, text, model_name=''):
+        if model_name == '':
+            self.autocomplete.models.train_models(text, model_name=False)
+        else:
+            self.autocomplete.models.train_models(text, model_name=model_name)
+        self.autocomplete.load()
         
-    def save_label_encoder(self):
-        with open("hmm_label_encoder.pkl", "wb") as f:
-            pickle.dump(self.label_encoder, f)
-
-    def load_hmm(self, label_encoder_path, hmm_model_path):
-        with open(label_encoder_path, "rb") as f:
-            label_encoder = pickle.load(f)
-        model = joblib.load(hmm_model_path)
-        return label_encoder, model
-
-    def predict_proba_hmm(self, word):
-        features, lengths = self.encode_text(word)
-        return model.predict_proba(features, lengths=lengths)
-        
-    def correction(self, word):
-        candidates = self._spell_checker.candidates(word)
-        probabilities = []
-        print(candidates)
-        for candidate in candidates:
-            try:
-                probabilities.append(
-                    self.predict_proba_hmm(candidate)
-                )
-            except:
-                continue
-        if probabilities == []:
-            raise Exception("""No replacement suggestions found. 
-            Consider using a bigger corpus.""")
-        index = np.argmax(probabilities)
-        return candidates[index]
-        
+    def correction(self, previous_word, word):
+        if self._spell_checker.known([word]):
+            return word
+        else:
+            spell_checker_candidates = self._spell_checker.candidates(word)
+            autocomplete_predictions = self.autocomplete.predict(previous_word, word[0])
+            autocomplete_candidates = [elem[0] for elem in autocomplete_predictions]
+            best_choices = []
+            for candidate in spell_checker_candidates:
+                try:
+                    candidate_index = autocomplete_candidates.index(candidate)
+                    best_choices.append(autocomplete_predictions[candidate_index])
+                except:
+                    continue
+            if best_choices:
+                best_choices = sorted(best_choices, key=lambda t:t[1])
+                return list(best_choices[-1])[0]
+            else:
+                return random.choice(spell_checker_candidates)
